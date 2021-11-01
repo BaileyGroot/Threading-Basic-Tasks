6ceddf7280374f2ed27da8c194fe01fefc982707
Markus Bailey Stockfleth Thomsbæk Groot <mark4805@zbc.dk>
2021-11-01 16:00:05 +0100

    Add project files.

---
+++ Threading Basic 0/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+class program
+{
+    public void WorkThreadFunction()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(Thread.CurrentThread.Name);
+        }
+    }
+}
+class threprog
+{
+    public static void Main()
+    {
+        program pg = new program();
+        Thread thread = new Thread(new ThreadStart(pg.WorkThreadFunction));
+        thread.Name = "Frederik";
+        thread.Start();
+
+        program pg2 = new program();
+        Thread thread2 = new Thread(pg.WorkThreadFunction);
+        thread2.Name = "Markus";
+        thread2.Start();
+
+
+        Console.Read();
+    }
+}
